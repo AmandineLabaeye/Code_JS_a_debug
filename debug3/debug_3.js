f065149f518d381d32dd72f3f6399d6eb94c85f3
sstienface <s.stien@fondationface.org>
2018-10-18 22:25:38 +0200

    Add debug3

---
+++ debug3/debug_3.js
@@ -0,0 +1,34 @@
+/**
+ * Created by sstienface on 15/10/2018.
+ */
+
+var texte = document.getElementById('main').innerHTML = "test";
+
+var boucle;
+while(var boucle<5)
+{
+    boucle=+5;
+}
+
+
+document.getElementById('divInconnuAuBataillon').innerHTML = "ttttttttttttttttt";
+
+if(variableIconnueAuBataillon == 1)
+{
+alert('connue');
+}
+else
+{
+    alert(inconnue);
+}
+
+
+alert("Penser à ne pas oublier...);
+
+
+alert "certaines choses";
+
+
+
+
+
